d64f7f9815c75f2e46ba7e62577e6d0f289698a7
Tolu Adesina <tadesina90@gmail.com>
2019-10-07 16:53:34 +0100

    added nebular theming to the app

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,12 +3,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button *ngIf="!value">{{ value }}</button>
-    <button *ngIf="value == 'X'">{{ value }}</button>
-    <button *ngIf="value == 'O'">{{ value }}</button>
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
-  
+
 })
 export class SquareComponent {
  
